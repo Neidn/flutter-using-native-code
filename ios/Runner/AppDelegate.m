4f14d10b88b9c4fb32793ee2d0131ada67bd871b
Neidn <dty3152@gmail.com>
2023-03-30 16:27:28 +0900

    write ios native code for getting battery

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,13 +1,49 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
+#import <Flutter/Flutter.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+      FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
+
+      FlutterMethodChannel* batteryChannel = [FlutterMethodChannel
+                                              methodChannelWithName:@"com.example.myapp/battery"
+                                              binaryMessenger:controller.binaryMessenger];
+
+      [batteryChannel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result) {
+        // Note: this method is invoked on the UI thread.
+        // Handle battery messages.
+        if ([@"getBatteryLevel" isEqualToString:call.method]) {
+          int batteryLevel = [self getBatteryLevel];
+
+          if (batteryLevel == -1) {
+            result([FlutterError errorWithCode:@"UNAVAILABLE"
+                                       message:@"Battery info unavailable"
+                                       details:nil]);
+          } else {
+            result(@(batteryLevel));
+          }
+        } else {
+          result(FlutterMethodNotImplemented);
+        }
+      }];
+
+
+    [GeneratedPluginRegistrant registerWithRegistry:self];
+    // Override point for customization after application launch.
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (int)getBatteryLevel {
+  UIDevice* device = UIDevice.currentDevice;
+  device.batteryMonitoringEnabled = YES;
+  if (device.batteryState == UIDeviceBatteryStateUnknown) {
+    return -1;
+  } else {
+    return (int)(device.batteryLevel * 100);
+  }
 }
 
 @end
